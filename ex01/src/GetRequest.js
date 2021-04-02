bd6ad866abacc07470bf77096a9d598dfbea95e3
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-04-02 16:23:33 +0200

    Added ex01

--- ex01/src/GetRequest.js
+++ ex01/src/GetRequest.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-class GetRequest extends React.Component {
+class GetRequest extends Component {
     constructor(props) {
         super(props);
         this.state = {
