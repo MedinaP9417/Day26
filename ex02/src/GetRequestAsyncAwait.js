852e555d81f8c8bfd6fdd5b0a0e213ae5d1cf9ef
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-04-02 16:28:48 +0200

    Added ex02

--- ex02/src/GetRequestAsyncAwait.js
+++ ex02/src/GetRequestAsyncAwait.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-class GetRequestAsyncAwait extends React.Component {
+class GetRequestAsyncAwait extends Component {
     constructor(props) {
         super(props);
         this.state = {
